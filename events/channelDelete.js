e878c7cc1e76d2482fcd5d349f31fffaa2b7e712
Joshua Gridley <jgrid25@gmail.com>
2017-04-17 16:44:37 -0500

    Fixed channelDelete.js in events.

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,4 +1,4 @@
-module.exports = (client, channel) => {
+module.exports = (channel, client, guild) => {
     console.log(`A ${channel.type} channel by the name of ${channel.name} was deleted.`);
     channel.guild.defaultChannel.sendMessage(`The ${channel.name} channel was deleted! :no_mouth:`);
 };
